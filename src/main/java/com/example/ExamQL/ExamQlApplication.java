fa750aaa7c970cd624d54026a8760debafef479b
soukaina <moustafilsoukaina@gmail.com>
2021-12-19 15:44:27 +0100

    init project

---
+++ src/main/java/com/example/ExamQL/ExamQlApplication.java
@@ -0,0 +1,13 @@
+package com.example.ExamQL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamQlApplication.class, args);
+	}
+
+}
